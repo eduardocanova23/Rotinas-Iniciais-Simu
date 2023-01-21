b392db82d93d8222c86a2501c1224f643f9242f7
Arthur Santâna <arthur.ea@hotmail.com>
2023-01-21 18:31:42 -0300

    cleaning stuff, adding debug stuff

--- Rotinas Iniciais Simu/q_learning.cs
+++ Rotinas Iniciais Simu/q_learning.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq.Expressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,33 +12,30 @@ namespace Rotinas_Iniciais_Simu
   public class q_learning
   {
     Random rnd;
-    float gamma = 1.0f;
-    float exploration_max = 1.0f;
-    float exploration_min = 0.01f;
-    float exploration_decay = 0.9999f;
-    float learning_rate_Q = 0.3f;
-    int n_outputs = 4;
-    int n_inputs = 10;
-    int episodes = (int)1e5;
+    float gamma;
+    float exploration_max;
+    float exploration_min;
+    float exploration_decay;
+    float learning_rate_Q;
+    int n_outputs;
+    int n_inputs;
+    int episodes;
+    int max_episodes;
+    float[,,,,,,,,,,] qTable;
 
     public q_learning()
     {
       Random rnd = new();
       int memory_size = Convert.ToInt32(5 * Math.Pow(10, 5));
-      int batch_size = 32;
-      float gamma = 1.0f;
-      float exploration_max = 1.0f;
-      float exploration_min = 0.01f;
-      float exploration_decay = 0.9f;
-      float learning_rate = 0.003f;
-      float learning_rate_Q = 0.3f;
-      float tau = 0.125f;
-      int n_outputs = 4;
-      int n_inputs = 10;
-      int layers = 2;
-      int neurons = 32;
-      int max_episodes = (int)1e5;
-
+      gamma = 1f;
+      exploration_max = 1f;
+      exploration_min = 0.0f;
+      exploration_decay = 0.999f;
+      learning_rate_Q = 0.5f;
+      n_outputs = 4;
+      n_inputs = 10;
+      max_episodes = (int)1e5;
+      qTable = makeQtable();
       loop(max_episodes, rnd);
     }
 
@@ -77,13 +76,16 @@ namespace Rotinas_Iniciais_Simu
           action = a;
         }
       }
+      if (action == -1)
+        throw new Exception("Action with invalid value/was not assigned");
       return action;
     }
 
-    void print_environment(float[,] state, float[,] ns, simple_env env, float reward, float ep_reward, int episode)
+    void print_environment(float[,] state, float[,] ns, simple_env env, float reward, float ep_reward, int episode, float old_state_Q, float new_state_Q)
     {
       Console.Clear();
       int pos = (int)state[0, 0];
+      Console.WriteLine("Rewards: ");
       for (int i = 1; i <= 16; i++)
       {
         float[,] next_state = (float[,])state.Clone();
@@ -102,11 +104,47 @@ namespace Rotinas_Iniciais_Simu
         if (i%4 == 0)
           Console.WriteLine();
       }
+      
       Console.WriteLine($"Episode: {episode}");
       Console.WriteLine($"Ep. Reward: {ep_reward}");
       Console.WriteLine($"Reward: {reward}");
       Console.WriteLine($"Exploration Rate: {exploration_max}");
-      System.Threading.Thread.Sleep(10);
+      string fmt = "{0,9:0.##}";
+      Console.Write("Old Q: ");
+      Console.WriteLine(fmt, old_state_Q);
+      Console.Write("New Q: ");
+      Console.WriteLine(fmt, new_state_Q);
+      Console.Write("Diff : ");
+      Console.WriteLine(fmt, old_state_Q - new_state_Q);
+
+
+      float max = float.MinValue;
+      foreach (var i in qTable)
+      {
+        if (i >= max)
+          max = i;
+      }
+      Console.WriteLine($"Max value of Q-Table: {max}");
+
+
+      //// Print Q table
+
+      //Console.WriteLine("Q-Table: ");
+      //var s = (float[,])state.Clone();
+      //for (int i = 1; i <= 16; i++)
+      //{
+      //  s[0, 0] = i;
+      //  Console.Write(String.Format("{0, 5}", argMaxTable(s, qTable)));
+      //  if (i % 4 == 0)
+      //    Console.WriteLine();
+      //}
+
+      // Print state action values
+      Console.WriteLine("State action values: L R U D");
+      for (int a = 0; a < 4; a++)
+        Console.Write(String.Format("{0, 5:0.00}", qTable[(int)state[0, 0] - 1, (int)state[0, 1], (int)state[0, 2], (int)state[0, 3], (int)state[0, 4], (int)state[0, 5], (int)state[0, 6], (int)state[0, 7], (int)state[0, 8], (int)state[0, 9], a]));
+
+      System.Threading.Thread.Sleep(50);
     }
 
     void loop(int max_episodes, Random rnd)
@@ -116,7 +154,6 @@ namespace Rotinas_Iniciais_Simu
       var (state, done) = env.reset();
       float[,] next_state;
       float reward;
-      var qTable = makeQtable();
       for (int ep = 0; ep < max_episodes; ep++)
       {
         float ep_reward = 0;
@@ -129,13 +166,12 @@ namespace Rotinas_Iniciais_Simu
           action = get_actionQ(state, rnd, qTable);
 
           (next_state, reward, done) = env.step(state, action);
-          print_environment(state, next_state, env, reward, ep_reward, ep);
-          ep_reward += reward;
+          
 
-          float state_value = qTable[(int)state[0, 0] - 1, (int)state[0, 1], (int)state[0, 2], (int)state[0, 3], (int)state[0, 4], (int)state[0, 5], (int)state[0, 6], (int)state[0, 7], (int)state[0, 8], (int)state[0, 9], action];
+          float old_state_Q = qTable[(int)state[0, 0] - 1, (int)state[0, 1], (int)state[0, 2], (int)state[0, 3], (int)state[0, 4], (int)state[0, 5], (int)state[0, 6], (int)state[0, 7], (int)state[0, 8], (int)state[0, 9], action];
           if (state[0, 0] <= 0 | next_state[0, 0] <= 0)
             throw new Exception(" state or next_state with position value incorrect.");
-
+          float new_state_Q = (reward + gamma * argMaxTable(next_state, qTable));
           qTable[
             (int)state[0, 0] - 1,
             (int)state[0, 1],
@@ -147,11 +183,13 @@ namespace Rotinas_Iniciais_Simu
             (int)state[0, 7],
             (int)state[0, 8],
             (int)state[0, 9],
-            action] = learning_rate_Q * (reward + gamma * argMaxTable(next_state, qTable) - state_value);
+            action] = (1-learning_rate_Q) * old_state_Q  + learning_rate_Q * new_state_Q;
+          
+          print_environment(state, next_state, env, reward, ep_reward, ep, old_state_Q, new_state_Q);
+          ep_reward += reward;
           state = (float[,])next_state.Clone();
           
         }
-        Console.WriteLine($"Episode reward during training: {ep_reward}");
       }
     }
   }
